41380cf00b7d2f313391595a49a312549344f3d8
Omar Mohamed <60580509+omarfawzy1@users.noreply.github.com>
2023-08-25 21:26:03 +0300

    Add Run Code Button Visual

---
+++ src/main/kotlin/CodeStatus.kt
@@ -0,0 +1,3 @@
+enum class CodeStatus {
+    Running, Paused, Debugging, Stopped
+}
--- src/main/kotlin/NavBar.kt
+++ src/main/kotlin/NavBar.kt
@@ -2,32 +2,66 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Rounded
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
 @Composable
 fun CodeActionBar(modifier: Modifier){
-    Row(modifier){
-        RunStopButton()
+    Column(modifier, verticalArrangement = Arrangement.SpaceAround){
+        Row{
+            RunStopButton(CodeStatus.Stopped)
+            Spacer(Modifier.size(10.dp))
+            DebugButton(CodeStatus.Stopped)
+        }
     }
 }
 @Composable
-fun RunStopButton(){
-    Box{
-        FloatingActionButton({}, ){
-
+fun RunStopButton(status: CodeStatus){
+    FloatingActionButton({}, shape = RoundedCornerShape(100),
+        modifier = Modifier.size(35.dp),
+        backgroundColor = Color.White){
+        when(status){
+            CodeStatus.Running -> Icon(Rounded.Home,"")
+            CodeStatus.Paused -> Icon(Rounded.Add,"")
+            CodeStatus.Debugging -> Icon(Rounded.Share,"")
+            CodeStatus.Stopped -> Icon(Rounded.Notifications,"")
         }
+
+    }
+}
+
+@Composable
+fun DebugButton(status: CodeStatus){
+    FloatingActionButton({}, shape = RoundedCornerShape(100),
+        modifier = Modifier.size(35.dp),
+        backgroundColor = Color.White){
+        when(status){
+            CodeStatus.Running -> Icon(Rounded.Home,"")
+            CodeStatus.Paused -> Icon(Rounded.Add,"")
+            CodeStatus.Debugging -> Icon(Rounded.Share,"")
+            CodeStatus.Stopped -> Icon(Rounded.Notifications,"")
+        }
+
     }
 }
 
 @Composable
 fun NavBar(modifier: Modifier){
     Row(modifier.fillMaxSize().background(Color.DarkGray)){
-        Row(Modifier.weight(0.5f).fillMaxHeight(), verticalAlignment = Alignment.CenterVertically){
+        Row(Modifier.weight(0.6f).fillMaxHeight(), verticalAlignment = Alignment.CenterVertically){
             Spacer(Modifier.weight(0.05f))
             Box(Modifier.size(Dp(35f)).clip(RoundedCornerShape(100)).background(Color.White))
             Spacer(Modifier.weight(0.5f))
@@ -37,10 +71,9 @@ fun NavBar(modifier: Modifier){
             Spacer(Modifier.weight(0.1f))
             Text("Help", color = Color.White)
         }
-        Row(Modifier.weight(0.5f).fillMaxHeight(), horizontalArrangement = Arrangement.End, verticalAlignment = Alignment.CenterVertically){
-            Column(Modifier.weight(1f)) {
-
-            }
+        Spacer(Modifier.weight(0.1f))
+        Row(Modifier.weight(0.4f).fillMaxHeight(), horizontalArrangement = Arrangement.End, verticalAlignment = Alignment.CenterVertically){
+            CodeActionBar(Modifier.weight(1f))
             Box(Modifier.size(Dp(35f)).clip(RoundedCornerShape(100)).background(Color.White))
             Spacer(Modifier.weight(0.05f))
         }
